b94422b34e9b2020d6cdffe206563203f29a6a74
Jason_Chen9 <995975102@qq.com>
2023-03-23 05:20:00 +0000

    fix tf2torch

--- gpt2/tf2flow.py
+++ gpt2/tf2flow.py
@@ -1,48 +1,41 @@
-from transformers import GPT2Tokenizer, TFGPT2LMHeadModel
+from transformers import TFGPT2LMHeadModel
 import tensorflow as tf
 import tf2onnx
 import torch
-from onnx2torch import convert
 import os
-text = "a dog"
-tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+import onnxruntime as rt
+import numpy as np
+import onnx
+from onnx2pytorch import ConvertModel
+
 
 def save_tf_model_as_onnx():
-    model = TFGPT2LMHeadModel.from_pretrained(
-        "gpt2", pad_token_id=tokenizer.eos_token_id
-    )
+    model = TFGPT2LMHeadModel.from_pretrained("gpt2")
 
-    input = tokenizer.encode(text, return_tensors = 'tf')
-    tf.random.set_seed(0)
-    sample_output = model.generate(
-        input, 
-        do_sample=True, 
-        max_length=97, 
-        top_p=0.78, 
-        top_k=0)
-    print(sample_output[0])
-    print(sample_output[0].shape)
-
-    print(tokenizer.decode(sample_output[0], skip_special_tokens=True))
+    sample_output = model(
+        tf.ones([1,5], dtype=tf.int32))
+    print("tf: ",sample_output[0])
 
     model.save_pretrained("../model/checkpoints/tfgpt2model", saved_model=True)
 
-    command = "python -m tf2onnx.convert --saved-model ../model/checkpoints/tfgpt2model/saved_model/1 --opset 11  --output ../model/tf2flow_gpt2.onnx"
+    command = "python -m tf2onnx.convert --saved-model ../model/checkpoints/tfgpt2model/saved_model/1 --opset 10  --output ../model/tf2flow_gpt2.onnx"
 
     os.system(command)
 
 def load_onnx_to_flow_model():
-    torch_model = convert("../model/tf2flow_gpt2.onnx")
-
-    input = tokenizer.encode(text, return_tensors = 'pt')
-    shape = input.shape
-
-    output = torch_model(
-        torch.ones(shape, dtype=torch.int32),input
+    onnx_model = onnx.load("../model/tf2flow_gpt2.onnx")
+    pytorch_model = ConvertModel(onnx_model)
+    output = pytorch_model(
+        attention_mask=torch.ones([1,5], dtype=torch.int32),
+        input_ids=torch.ones([1,5], dtype=torch.int32)
     )
-    print(output[0])
-    print(output[0].shape)
-    # print(tokenizer.decode(output[0], skip_special_tokens=True))
+
+    providers = ['CPUExecutionProvider']
+    m = rt.InferenceSession("../model/tf2flow_gpt2.onnx", providers=providers)
+    onnx_pred = m.run(['logits', 'past_key_values'], {"attention_mask": np.ones([1,5], dtype=np.int32),"input_ids":np.ones([1,5], dtype=np.int32)})
+    
+    np.testing.assert_allclose(output[0].detach().numpy(), onnx_pred[0], rtol=1e-2)
+    print("pass")
 
 
 if __name__ == '__main__':
