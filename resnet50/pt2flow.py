7da6f829eeef9dba701cf7c4990c73a8536e9b9e
Jason_Chen9 <995975102@qq.com>
2023-03-29 14:56:13 +0000

    add resnet50 pt2onnx

--- resnet50/pt2flow.py
+++ resnet50/pt2flow.py
@@ -1,23 +1,47 @@
-import torch
 from torchvision.io import read_image
 from torchvision.models import resnet50, ResNet50_Weights
+import torch 
+import os
+import onnx
+from onnx2pytorch import ConvertModel
+import oneflow.mock_torch as mock
 
 img = read_image("../img/cat.jpg")
 
-# Step 1: Initialize model with the best available weights
-weights = ResNet50_Weights.DEFAULT
-model = resnet50(weights=weights)
-model.eval()
+def save_pt_model_as_onnx():
+    weights = ResNet50_Weights.DEFAULT
+    model = resnet50(weights=weights)
+    model.eval()
 
-# Step 2: Initialize the inference transforms
-preprocess = weights.transforms()
+    preprocess = weights.transforms()
 
-# Step 3: Apply inference preprocessing transforms
-batch = preprocess(img).unsqueeze(0)
+    batch = preprocess(img).unsqueeze(0)
 
-# Step 4: Use the model and print the predicted category
-prediction = model(batch).squeeze(0).softmax(0)
-class_id = prediction.argmax().item()
-score = prediction[class_id].item()
-category_name = weights.meta["categories"][class_id]
-print(f"{category_name}: {100 * score:.1f}%")
+    # prediction = model(batch).squeeze(0).softmax(0)
+    prediction = model(batch)
+    torch.onnx.export(model, batch, '../model/pt2flow_resnet50')
+
+    # class_id = prediction.argmax().item()
+    # score = prediction[class_id].item()
+    # category_name = weights.meta["categories"][class_id]
+    # print(category_name)
+
+def load_onnx_model_as_flow():
+    # os.system("eval $(oneflow-mock-torch --lazy --verbose)")
+    # onnx_model = onnx.load("../model/pt2flow_resnet50")
+    # pytorch_model = ConvertModel(onnx_model)
+    # output = pytorch_model(
+    #     batch
+    # )
+
+    # print(output)
+
+    # # np.testing.assert_allclose(output[0].detach().numpy(), onnx_pred[0], rtol=1e-2)
+    print("pass")
+    # os.system("eval $(oneflow-mock-torch disable)")
+
+if __name__ == '__main__':
+    save_pt_model_as_onnx()
+    with mock.enable():
+        load_onnx_model_as_flow()
+
