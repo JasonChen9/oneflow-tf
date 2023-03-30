bb516224b9de6fde6601a9b068cc81cb7ab8f379
Jason_Chen9 <995975102@qq.com>
2023-03-30 02:22:23 +0000

    refine gpt2 pt2flow

--- gpt2/pt2flow.py
+++ gpt2/pt2flow.py
@@ -1,25 +1,22 @@
+import numpy as np
 import torch
 from transformers import GPT2LMHeadModel
-import os
-import onnxruntime as rtE
-import numpy as np
-import onnx
-import oneflow.mock_torch as mock
+import oneflow as flow
 
-img_path = "../img/cat.jpg"
-onnx_model_path = "../model/pt2flow_gpt2.onnx"
 def save_pt_model_as_onnx():
     model = GPT2LMHeadModel.from_pretrained("gpt2")
     sample_input = torch.ones([1, 5], dtype=torch.int32)
-    output = model(sample_input)
+    output = model(sample_input).logits.detach().numpy()
+    
+    if(isinstance(torch.zeros(2, 3), flow.Tensor)):
+        path = "../model/pt2flow_gpt2_flow.npy"
+    else:
+        path = "../model/pt2flow_gpt2_pt.npy"
+    print(path)
+    np.save(path, output)
     return output
 
 if __name__ == '__main__':
-    pt_out = save_pt_model_as_onnx()
+    save_pt_model_as_onnx()
 
-    # with mock.enable(lazy=True, verbose=True):
-    #     flow_out = save_pt_model_as_onnx()
-    # np.testing.assert_allclose(pt_out, flow_out, rtol=1e-2, atol=2e-5)
-    # print(pt_out)
-    print("PASS")
 
