c7ab595bfd8218d5eda79609ed0e512e059aad43
Jason_Chen9 <995975102@qq.com>
2023-03-30 02:31:45 +0000

    add gpt2 pt2flow test

---
+++ gpt2/test_pt2flow.py
@@ -0,0 +1,11 @@
+import os
+import numpy as np
+
+os.system("python3 pt2flow.py")
+torch_output = np.load("../model/pt2flow_gpt2_pt.npy")
+
+os.system("eval $(oneflow-mock-torch --lazy) && python3 pt2flow.py")
+flow_output = np.load("../model/pt2flow_gpt2_flow.npy")
+
+np.testing.assert_allclose(torch_output, flow_output, rtol=1e-05, atol=1e-05)
+print("PASS")
